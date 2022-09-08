674f17ec392c439dec1946f00637442522db665d
solocldev <113052678+solocldev@users.noreply.github.com>
2022-09-08 13:18:16 +0530

    responding with an object

--- Routes/getObject.js
+++ Routes/getObject.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
     try {
         const object = await Object.aggregate([{ $sample: { size: 1 } }])
-        res.json(object)
+        res.json(object[0])
     } catch (error) {
         console.log(error);
         res.send(error)
